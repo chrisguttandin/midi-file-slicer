468d94bbd41bbdb40528c6536f327925835a0568
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-07 21:40:47 +0200

    setup travis tests

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -4,47 +4,73 @@ var browserify = require('../../package.json').browserify;
 
 module.exports = function (config) {
 
-    config.set({
+    var configuration = {
 
-        basePath: '../../',
+            basePath: '../../',
 
-        browserify: {
-            transform: browserify.transform
-        },
+            browserify: {
+                transform: browserify.transform
+            },
 
-        browsers: [
+            files: [
+                'src/midi-file-slicer.js',
+                {
+                    included: false,
+                    pattern: 'src/**/*.js',
+                    served: false,
+                    watched: true,
+                },
+                {
+                    included: false,
+                    pattern: 'test/fixtures/**',
+                    served: true,
+                    watched: true,
+                },
+                'test/unit/**/*.js'
+            ],
+
+            frameworks: [
+                'browserify',
+                'mocha',
+                'sinon-chai' // implicitly uses chai too
+            ],
+
+            preprocessors: {
+                'src/midi-file-slicer.js': 'browserify',
+                'test/unit/**/*.js': 'browserify'
+            }
+
+        };
+
+    if (process.env.TRAVIS) { // jshint ignore: line
+        configuration.browsers = [
+            'ChromeSauceLabs',
+            'FirefoxSauceLabs'
+        ];
+
+        configuration.captureTimeout = 120000;
+
+        configuration.customLaunchers = {
+            ChromeSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11'
+            },
+            FirefoxSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11'
+            }
+        };
+
+        configuration.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER; // jshint ignore: line
+    } else {
+        configuration.browsers = [
             'ChromeCanary',
             'FirefoxDeveloper'
-        ],
+        ];
+    }
 
-        files: [
-            'src/midi-file-slicer.js',
-            {
-                included: false,
-                pattern: 'src/**/*.js',
-                served: false,
-                watched: true,
-            },
-            {
-                included: false,
-                pattern: 'test/fixtures/**',
-                served: true,
-                watched: true,
-            },
-            'test/unit/**/*.js'
-        ],
-
-        frameworks: [
-            'browserify',
-            'mocha',
-            'sinon-chai' // implicitly uses chai too
-        ],
-
-        preprocessors: {
-            'src/midi-file-slicer.js': 'browserify',
-            'test/unit/**/*.js': 'browserify'
-        }
-
-    });
+    config.set(configuration);
 
 };
