f14ba3e4f74546411fcb3add8a6bb3a6c6cbf4fc
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-01 23:45:03 +0200

    apply linting rules

--- src/midi-file-slicer.ts
+++ src/midi-file-slicer.ts
@@ -10,32 +10,6 @@ export class MidiFileSlicer {
         this._gatherMicrosecondsPerBeat();
     }
 
-    private _gatherMicrosecondsPerBeat () {
-        const tracks = this._json.tracks;
-
-        const length = tracks.length;
-
-        tracks: for (let i = 0; i < length; i += 1) {
-            const track = tracks[i];
-
-            const lngth = track.length;
-
-            for (let j = 0; j < lngth; j += 1) {
-                const event = track[j];
-
-                if (event.setTempo !== undefined) {
-                    this._microsecondsPerBeat = event.setTempo.microsecondsPerBeat;
-
-                    break tracks;
-                }
-            }
-        }
-
-        if (this._microsecondsPerBeat === undefined) {
-            this._microsecondsPerBeat = 500000;
-        }
-    }
-
     public slice (start, end) {
         const events = [];
 
@@ -71,6 +45,32 @@ export class MidiFileSlicer {
         }
 
         return events;
+    }
+
+    private _gatherMicrosecondsPerBeat () {
+        const tracks = this._json.tracks;
+
+        const length = tracks.length;
+
+        tracks: for (let i = 0; i < length; i += 1) {
+            const track = tracks[i];
+
+            const lngth = track.length;
+
+            for (let j = 0; j < lngth; j += 1) {
+                const event = track[j];
+
+                if (event.setTempo !== undefined) {
+                    this._microsecondsPerBeat = event.setTempo.microsecondsPerBeat;
+
+                    break tracks;
+                }
+            }
+        }
+
+        if (this._microsecondsPerBeat === undefined) {
+            this._microsecondsPerBeat = 500000;
+        }
     }
 
 }
