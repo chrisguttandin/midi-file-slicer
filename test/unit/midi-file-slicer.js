48735da5a3b3cdb573d141aebde1b3c2b8364f77
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-15 11:32:50 +0100

    test: add files with separate tracks

--- test/unit/midi-file-slicer.js
+++ test/unit/midi-file-slicer.js
@@ -1,7 +1,9 @@
 import { MidiFileSlicer } from '../../src/midi-file-slicer';
 import multipleSetTempoEvents from '../fixtures/multiple-set-tempo-events.json';
+import multipleSetTempoEventsOnASeparateTrack from '../fixtures/multiple-set-tempo-events-on-a-separate-track.json';
 import noSetTempoEvent from '../fixtures/no-set-tempo-event.json';
 import oneSetTempoEvent from '../fixtures/one-set-tempo-event.json';
+import oneSetTempoEventOnASeparateTrack from '../fixtures/one-set-tempo-event-on-a-separate-track.json';
 
 describe('MidiFileSlicer', () => {
     describe('slice()', () => {
@@ -79,6 +81,53 @@ describe('MidiFileSlicer', () => {
             ]);
         });
 
+        it('should slice a midi representation with one set tempo event on a separate track', () => {
+            const midiFileSlicer = new MidiFileSlicer({ json: oneSetTempoEventOnASeparateTrack });
+
+            expect(midiFileSlicer.slice(500, 1000)).to.deep.equal([
+                {
+                    event: {
+                        delta: 240,
+                        noteOn: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 0
+                },
+                {
+                    event: {
+                        delta: 240,
+                        noteOff: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 125
+                },
+                {
+                    event: {
+                        delta: 240,
+                        noteOn: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 250
+                },
+                {
+                    event: {
+                        delta: 240,
+                        noteOff: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 375
+                }
+            ]);
+        });
+
         it('should slice a midi representation with multiple set tempo events', () => {
             const midiFileSlicer = new MidiFileSlicer({ json: multipleSetTempoEvents });
 
@@ -109,6 +158,59 @@ describe('MidiFileSlicer', () => {
                         setTempo: {
                             microsecondsPerQuarter: 500000
                         }
+                    },
+                    time: 125
+                },
+                {
+                    event: {
+                        delta: 240,
+                        noteOn: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 375
+                }
+            ]);
+        });
+
+        it('should slice a midi representation with multiple set tempo events on a separate track', () => {
+            const midiFileSlicer = new MidiFileSlicer({ json: multipleSetTempoEventsOnASeparateTrack });
+
+            expect(midiFileSlicer.slice(500, 1000)).to.deep.equal([
+                {
+                    event: {
+                        delta: 240,
+                        noteOn: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 0
+                },
+                {
+                    event: {
+                        delta: 1200,
+                        setTempo: {
+                            microsecondsPerQuarter: 500000
+                        }
+                    },
+                    time: 125
+                },
+                {
+                    event: {
+                        delta: 240,
+                        noteOff: {
+                            noteNumber: 60,
+                            velocity: 127
+                        }
+                    },
+                    time: 125
+                },
+                {
+                    event: {
+                        delta: 0,
+                        endOfTrack: true
                     },
                     time: 125
                 },
